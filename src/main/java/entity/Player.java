10a3f024d7d2661cc2c72ae1af083053d89492d8
Santiago Gómez <thiago.gomez1510@gmail.com>
2026-09-05 15:02:49 -0500

    Base del nuevo mapa

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -50,8 +50,8 @@ public class Player extends Entity{
     // Vamos a establecer las configuraciones del Player
     public void setDefaultValues(){
         // POSICIÓN INICIAL
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
+        worldX = gp.tileSize * 36;
+        worldY = gp.tileSize * 10;
         speed = 4;
         direction = "down";
         
--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import main.GamePanel;
 import main.UtilityTool;
@@ -17,64 +18,77 @@ public class TileManager {
     public int mapTileNum [] [] []; //Usaremos los números del archivo de texto para dibujarlo
     boolean drawPath = false;
     
+    ArrayList<String> fileNames = new ArrayList<>();
+    ArrayList<String> collisionStatus = new ArrayList<>();
+    
     public TileManager(GamePanel gp) {
         this.gp = gp;
-        tile = new Tile[50]; // Cantidad de diferentes bloques
-        mapTileNum = new int [gp.maxMap] [gp.maxWorldCol] [gp.maxWorldRow];  
+        
+        // READ TILE DATA FILE
+        InputStream is = getClass().getResourceAsStream("/maps/tiledata.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        
+        // GETTING TILE STATES AND COLLISION INFO FROM THE FILE
+        String line;
+        
+        try{
+            while((line = br.readLine()) != null){
+                fileNames.add(line);
+                collisionStatus.add(br.readLine());
+            }
+            
+            br.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        
+        // Inicializamos la base
+        tile = new Tile[fileNames.size()]; // Cantidad de diferentes bloques
         getTileImage();
-        loadMap("/maps/world01.txt", 0);
-        //loadMap("/maps/world02.txt", 1);
+        
+        
+        // Obtenemos el maxWorldCol & maxWorldRow
+        is = getClass().getResourceAsStream("/maps/worldmap.txt");
+        br = new BufferedReader(new InputStreamReader(is));
+        
+        try{
+            String line2 = br.readLine();
+            String maxFile[] = line2.split(" ");
+            
+            gp.maxWorldCol = maxFile.length;
+            gp.maxWorldRow = maxFile.length;
+            mapTileNum = new int [gp.maxMap] [gp.maxWorldCol] [gp.maxWorldRow];  
+            
+            br.close();
+            
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        
+        loadMap("/maps/worldmap.txt", 0);
+       
     }
     
     public void getTileImage(){
         
-        // Index, Image, Collision
-        // No usaremos las posiciones del 0 al 9
-        setup(0, "grass00", false);
-        setup(1, "grass00", false);
-        setup(2, "grass00", false);
-        setup(3, "grass00", false);
-        setup(4, "grass00", false);
-        setup(5, "grass00", false);
-        setup(6, "grass00", false);
-        setup(7, "grass00", false);
-        setup(8, "grass00", false);
-        setup(9, "grass00", false);
-        
-        // A partir de aquí usaremos los tiles
-        // Esto lo hago para que sea más facil de leer los números en el .txt
-        setup(10, "grass00", false);
-        setup(11, "grass01", false);
-        setup(12, "water00", false);
-        setup(13, "water01", true);
-        setup(14, "water02", true);
-        setup(15, "water03", true);
-        setup(16, "water04", true);
-        setup(17, "water05", true);
-        setup(18, "water06", true);
-        setup(19, "water07", true);
-        setup(20, "water08", true);
-        setup(21, "water09", true);
-        setup(22, "water10", true);
-        setup(23, "water11", true);
-        setup(24, "water12", true);
-        setup(25, "water13", true);
-        setup(26, "road00", false);
-        setup(27, "road01", false);
-        setup(28, "road02", false);
-        setup(29, "road03", false);
-        setup(30, "road04", false);
-        setup(31, "road05", false);
-        setup(32, "road06", false);
-        setup(33, "road07", false);
-        setup(34, "road08", false);
-        setup(35, "road09", false);
-        setup(36, "road10", false);
-        setup(37, "road11", false);
-        setup(38, "road12", false);
-        setup(39, "earth", false);
-        setup(40, "wall", true);
-        setup(41, "tree", true);
+        for(int i = 0; i < fileNames.size(); i++){
+            
+            String fileName;
+            boolean collision;
+            
+            // Get fileName
+            fileName = fileNames.get(i);
+            
+            // Get a collision status
+            if(collisionStatus.get(i).equals("true")){
+                collision = true;
+            } else{
+                collision = false;
+            }
+            
+            setup(i, fileName, collision);
+            
+        }
     }
     
     // Esta función va a optimizar la creación de los mosaicos y su escala
@@ -83,7 +97,7 @@ public class TileManager {
         
         try{
             tile[index] = new Tile();
-            tile[index].image = ImageIO.read(getClass().getResourceAsStream("/tiles/"+image+".png"));
+            tile[index].image = ImageIO.read(getClass().getResourceAsStream("/tiles/"+image));
             tile[index].image = uTool.scaleImage(tile[index].image, gp.tileSize, gp.tileSize);
             tile[index].collision = collision;
             
@@ -102,9 +116,9 @@ public class TileManager {
                     
             while(col < gp.maxWorldCol && row < gp.maxWorldRow){
                 String  line = br.readLine();
-                while(col < gp.maxWorldCol){
+               while(col < gp.maxWorldCol){
                     String numbers[] = line.split(" ");
-                    int num = Integer.parseInt(numbers[col]);
+                     int num = Integer.parseInt(numbers[col]);
                     mapTileNum[map][col][row] = num;
                     col++;
                 }
